19458fc71aa32eb9c7b443d7cdce28fe52c0030e
Project Manager <84538339+PripyatAutomations@users.noreply.github.com>
2024-01-08 10:53:55 -0500

    adding color selection dialog

--- lib/rustyrigs_set_colors.pm
+++ lib/rustyrigs_set_colors.pm
@@ -7,12 +7,23 @@ use warnings;
 
 my $window;
 
+sub cancel {
+   my ( $self ) = @_;
+}
+
+sub save {
+   my ( $self ) = @_;
+}
+
 sub DESTROY {
    my ( $self ) = @_;
 }
 
 sub new {
    my ( $class, $w_set_ref ) = @_;
+
+   my $cfg = $main::cfg;
+
    # settings window
    my $w_set = ${$w_set_ref};
    my $box;
@@ -37,16 +48,33 @@ sub new {
 
    my $icon = ${$gtk_ui->{'icon_settings_pix'}};
    $window->set_icon($icon);
+
+   my $accel = Gtk3::AccelGroup->new();
+   $window->add_accel_group($accel);
    $box = Gtk3::Box->new('vertical', 5);
+
+   my $button_box = Gtk3::Box->new('horizontal', 5);
+   my $save_button = Gtk3::Button->new("_Save");
+   $save_button->set_tooltip_text("Save settings");
+   $save_button->set_can_focus(1);
+   $accel->connect(ord('S'), $$cfg->{'shortcut_key'}, 'visible', sub { my ( $self ) = @_; $self->save(); });
+   $button_box->pack_start($save_button, TRUE, TRUE, 0);
+
+   my $cancel_button = Gtk3::Button->new("_Cancel");
+   $cancel_button->set_tooltip_text("Cancel");
+   $cancel_button->set_can_focus(1);
+   $accel->connect(ord('C'), $$cfg->{'shortcut_key'}, 'visible', sub { my ( $self ) = @_; $self->cancel(); });
+   $button_box->pack_start($cancel_button, TRUE, TRUE, 0);
+   $box->pack_end($button_box, FALSE, FALSE, 0);
+
    $window->add($box);
-
-   # Add the widgets here...
-
    $window->show_all();
 
    my $self = {
       # functions
       # variables
+      accel => \$accel,
+      box => \$box,
       window => \$window
    };
 
