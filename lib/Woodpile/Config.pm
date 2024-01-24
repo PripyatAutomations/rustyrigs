ee9c21777d4da2742b2ea97d191c02ad6d0dd998
Netrig Developers <netrig@istabpeople.com>
2024-01-24 16:25:12 -0500

    fixed

--- lib/Woodpile/Config.pm
+++ lib/Woodpile/Config.pm
@@ -17,7 +17,7 @@ sub apply {
     if ($save) {
        # Save to the configuration file
        my $cfg_file = $self->{'cfg_file'};
-       $self->save($$cfg_file);
+       $self->save( $self->{'cfg'}, $$cfg_file );
     }
     return;
 }
@@ -25,13 +25,20 @@ sub apply {
 sub load {
     my ( $self, $cfg_file, $def_cfg ) = @_;
     my $rv;
+    my $use_defaults = 0;
+
+    if ( ! -f $$cfg_file ) {
+       print "Saving defaults to $$cfg_file since it doesn't exist\n";
+       save( $self, $def_cfg, $$cfg_file );
+       $rv = $def_cfg;
+       $use_defaults = 1;
+    }
 
     # If a config file exists, load it
-    if ( -f $$cfg_file ) {
+    if ( -f $$cfg_file && !$use_defaults ) {
         open my $fh, '<', $$cfg_file
           or die "Can't open config file $$cfg_file for reading: $!";
-        $main::log->Log( "config", "info",
-            "loading configuration from $$cfg_file" );
+        $main::log->Log( "config", "info", "loading configuration from $$cfg_file" );
         my $yaml_content = do { local $/; <$fh> };
         my $new_cfg      = YAML::XS::Load($yaml_content);
 
@@ -48,19 +55,15 @@ sub load {
             $rv = $$def_cfg;
         }
     }
-    else {
-        warn "[config/info] cant find config file $$cfg_file, using defaults\n";
-        $rv = $$def_cfg;
-    }
     return $rv;
 }
 
 sub save {
-    my ( $self, $cfg_file ) = @_;
+    my ( $self, $save_cfg, $cfg_file ) = @_;
 
     if ( !$cfg_readonly ) {
         print "[core/debug] saving config to $cfg_file\n";
-        my $cfg_out_txt = YAML::XS::Dump( $self->{cfg} );
+        my $cfg_out_txt = YAML::XS::Dump( $save_cfg );
         if ( !defined($cfg_out_txt) ) {
             die "Exporting YAML configuration failed\n";
         }
@@ -70,8 +73,7 @@ sub save {
         close $fh;
     }
     else {
-        $self->log->Log( "core", "info",
-            "not saving configuration $cfg_file as cfg_readonly is enabled..." );
+        $self->log->Log( "core", "info", "not saving configuration $cfg_file as cfg_readonly is enabled..." );
     }
     return;
 }
